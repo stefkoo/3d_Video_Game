0eb47325354226b7aa6df59068b85907840b2ca5
sttait00 <sttait00@hs-esslingen.de>
2023-06-15 15:40:47 +0200

    scale rotate

--- js/scene.js
+++ js/scene.js
@@ -59,11 +59,11 @@ export function create_geometry(scene) {
       mod = gltf.scene;
       cursor.add(mod);
       gltf.scene.scale.set(0.055, 0.055, 0.055);
-      gltf.scene.position.y = -0.185;
+      gltf.scene.position.y = -0.199;
       gltf.scene.position.x = +0.025;
       //   gltf.scene.position.z = -0.5;
-      gltf.scene.rotation.x = Math.PI / -2;
-      gltf.scene.rotation.y = Math.PI - 0.1;      //   gltf.scene.rotation.z = 80;
+      gltf.scene.rotation.x = Math.PI / 9;
+      gltf.scene.rotation.y = Math.PI / 0.5;      //   gltf.scene.rotation.z = 80;
       gltf.scene.updateMatrix();
       //   scene.add(mod);
     },
